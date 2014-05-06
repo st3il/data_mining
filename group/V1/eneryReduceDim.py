98ccf355fc0a2ee1a7c0421036dd617541b1a742
st3il <st3il@web.de>
2014-05-06 12:08:17 +0200

    V1: 2.2.2: comments added

--- group/V1/eneryReduceDim.py
+++ group/V1/eneryReduceDim.py
@@ -1,23 +1,27 @@
+## IMPORT LIBRARIES ###
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import manifold
+
+
 ### FUNCTIONS START ###
 
 def createIsomap(energyDataFrame):
+    #get oil coal gas nuclear and hydro column
     cleanedEnergyDataFrame = energyDataFrame[['Oil', 'Coal', 'Gas', 'Nuclear', 'Hydro']]
+    #get country column
     countrynames = np.array(energyDataFrame[['Country']])
-
-    countrynames = countrynames[:]
-
+    #get number of countries
     numOfInstances = len(countrynames)
-
+    #create isomap
     imap = manifold.Isomap()
-
+    #add data to isomap
     X = imap.fit_transform(cleanedEnergyDataFrame)
-
+    #create plot
     plt.plot(X[:,0],X[:,1],'.')
     for z in range(numOfInstances):
+        #add text
         plt.text(X[z,0], X[z,1], str(countrynames[z]))
     plt.show()
 
@@ -26,5 +30,6 @@ def createIsomap(energyDataFrame):
 #read file
 energyDataFrame = pd.read_csv("data/EnergyMix.csv")
 
+#createIsomap function call
 createIsomap(energyDataFrame)
 
