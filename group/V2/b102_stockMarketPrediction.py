96e239b3b3b7f4baf347b2317a76faaef05b9718
jackdecrack <jackdecrack@web.de>
2014-05-21 18:05:03 +0200

    test

--- group/V2/b102_stockMarketPrediction.py
+++ group/V2/b102_stockMarketPrediction.py
@@ -8,7 +8,7 @@ from sklearn.metrics import mean_absolute_error
 from sklearn import cross_validation
 
 
-dateiinhalt = pd.read_csv("src/effectiveRates.csv")
+dateiinhalt = pd.read_csv("data/effectiveRates.csv")
 
 #print("effectiveRates");
 #print(dateiinhalt);
@@ -27,36 +27,30 @@ print(yahoo)
 
 
 #time delay: 24  Anzahl der zu berucksichtigten vorhergehenden Kurswerte
-timeDelay = 24
+#timeDelay = 24
+timeDelay = 34
+
+#Anlegen einer SVR  mit RBF Kernel (empfohlene Parameter: C = 500, epsilon = 0,6)
 svrC = 500.0
-svrE = 1.8
+#svrE = 0.6
+svrE = 1.4 #optimaler Wert: 1,4
 
 
 #
-def createCyclicData(data, timeDelay):
-    # create an array with the shape nrValues X timeDelay+Value
+def createCyclicData(data):
+    # Erzeuge Array in Form von Anzahl Werte x Anzahl der zu berucksichtigten vorhergehenden Kurswerte + heutiger Wert
     result = np.zeros((len(data),timeDelay+1))
 
-    # timedelay too small, return current data
-    if timeDelay == 0:
-        return data
 
-    # go through each data-row
     for i in range(len(data)):
-        #print "i:\t\t%d" % i
-
-        # get the i-timeDelay values as input-attributes
+        # hole Werte vom jwl vorhergehenden Tage
         for j in range(timeDelay+1):
             # calculate index of item
             index = i - (timeDelay-j)
 
-            # ring around the rosy
             if index < 0:
                 index = len(data) + index
 
-            #print "index:\t%d" % index
-
-            # put value into result-index
             result[i,j] = data[index]
 
     return result
@@ -64,7 +58,7 @@ def createCyclicData(data, timeDelay):
 
 
 #Serielle Daten Yahoo
-cycleTableYahoo = createCyclicData(yahoo, timeDelay)
+cycleTableYahoo = createCyclicData(yahoo)
 
 print("cycleTableYahoo");
 print( cycleTableYahoo)
@@ -80,17 +74,10 @@ print( features)
 print( len(features))
 
 
-
-
-
-
-
-
-
 #plt.xticks(len(a), list(a))
-plt.plot(yahoo)
-
-plt.show()
+#plt.plot(yahoo)
+#plt.title("Yahoo Kurs")
+#plt.show()
 
 
 
@@ -102,41 +89,37 @@ plt.show()
 
 
 
-# Create SVR with given settings
-svr = SVR(C= svrC, epsilon= svrE, kernel='rbf')
+# Erzeuge SVR
+svr = SVR(C=svrC, epsilon=svrE, kernel='rbf')
 
-print "----------------------------Fitting---------------------------------------"
-# start fitting
+# fitting
 fittedData = svr.fit(features, targets)
 
-# data to predict: last 30 days
+# Vorhersagezeitraum
 predictDuration = 30
 predictedData = list()
 
-print "----------------------------Prediction---------------------------------------"
 for i in range(predictDuration):
 
     predictVector = np.zeros((timeDelay))
-    # count from 23 to 0
+
+    # von 24 bis 0
     for j in range(timeDelay-1,-1, -1):
-        # what is the index of the corresponding item in the predicted data?
+
         idxPredict = len(predictedData)-j-1
 
-        # if we are below 0, it means we do not have any more predicted values
+        # unter null haben wir keine vorhergesagten Werte
         if idxPredict >= 0:
             predictVector[j] = predictedData[idxPredict]
-            #print "From predicted %f" % predictVector[j]
             continue # could get value from predicted data -> use it!
 
-        # we could not fill the whole vector with already predicted data, fill it with already known data
+        # hole bekannte Werte, wenn wir den Datenbvektro mit verhergesagten werten füllen können
         idxTarget = 650 - j + len(predictedData)
         predictVector[j] = cycleTableYahoo[idxTarget, -1]
-       # print "From target %f" % predictVector[j]
 
-    # predict the data using trainings-data from the SVR
     predictedData.append(svr.predict(predictVector)[0])
 
-# stuff into a numpy array
+# umwandeln in Array
 predictedData = np.array(predictedData)
 
 # print data from 651 -> 680
@@ -145,16 +128,17 @@ print predictedData
 print "\nReal Values"
 print targets[len(targets)-predictDuration:]
 
-print "----------------------------MAD/MAE---------------------------------------"
-# calculate the absolute deviation
-mae = mean_absolute_error(targets[len(targets)-predictDuration:], predictedData)
-mad = 1.0/predictDuration*np.sum(np.abs(predictedData-targets[len(targets)-predictDuration:]))
+print "----------------------------MAE---------------------------------------"
+# Berechne Mean Absolute Error
+# MAE zeigt den mittleren Abweichwert
 
-print "\nMean Absolute Deviation:\t%0.3f" % mad
+#reale Daten der letzten 30 Tage (predictDuration) :
+compareRealYahooTargets = targets[len(targets)-predictDuration:]
+mae = mean_absolute_error(compareRealYahooTargets, predictedData)
+
 print "Mean Absolute Error:\t\t%0.3f" % mae
 
 print "----------------------------PLOT---------------------------------------"
-# plot everything out
 realForecast = cycleTableYahoo[649:(650+predictDuration), -1]
 plt.plot(range(650,650+predictDuration),predictedData, '-r', label="prediction")
 plt.plot(range(len(targets)), targets, '-k', label="real")
