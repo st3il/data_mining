f71b1c65cf5e1bcd1afec2127c88971c88a30fe5
jackdecrack <jackdecrack@web.de>
2014-06-15 16:26:05 +0200

    aufgabe 2.4

--- group/V4/docclass.py
+++ group/V4/docclass.py
@@ -10,6 +10,7 @@ class Classifier:
         self.cc['bad'] = 0
         self.getfeatures = fn
 
+    #increments word counter
     def incf(self,f,cat):
         #check f word is already in fc
 
@@ -47,14 +48,23 @@ class Classifier:
         words = self.getfeatures(item,3,20)
         for word in words:
             #add each word to cf
+            print "Word: '%s'" % word
+            print "added to: '%s'" % cat
             self.incf(word,cat)
         #increment category counter
         self.incc(cat)
 
     #return probability of given word in given category
     def fprob(self,f,cat):
+
+        fprobresult = 0.0
+
+        fcount = float(self.fcount(f,cat))
+        catcount = float(self.catcount(cat))
+
         if(self.catcount(cat) != 0):
-            return float(self.fcount(f,cat)) / float(self.catcount(cat))
+            fprobresult = fcount/catcount
+            return fprobresult
         else:
             return 0
 
@@ -62,11 +72,15 @@ class Classifier:
     def weightedprob(self,f,cat):
 
         initprob = float(0.5)
-        fcount = float(self.fcount(f,cat))
+
+
+        #fcount = float(self.fcount(f,cat)) ??
+        # total number of given word
+        fcount = self.fcount(f,"good") + self.fcount(f,"bad")
+
+
         fprob = float(self.fprob(f,cat))
-        print(fcount)
-        print(fprob)
-        result = float(initprob + fcount * fprob/(1 + fcount))
+        result = float((initprob + fcount * fprob)/(1 + fcount))
 
         print("Prob of Word '%s' in %s : %s" %(f, cat, result))
 
@@ -74,18 +88,23 @@ class Classifier:
 
     #return probability of given category
     def catprob(self,cat):
+        catporbresult = 0.0
         amountCat = float(self.catcount(cat))
         totalCount = float(self.totalcount())
-
-        return (amountCat / totalCount)
+        catporbresult = (amountCat / totalCount)
+        return catporbresult
 
 
     #returns probability if given document is in given category
     def prob(self,item,cat):
+        probresult = 0.0
         words = self.getfeatures(item, 3, 10)
-        tmp = 0
+
+        print(words)
+
+        tmp = 0.0
         for word in words:
-            if(tmp == 0):
+            if(tmp == 0.0):
                # print "temp init"
                 tmp = self.weightedprob(word,cat)
                # print tmp
@@ -94,7 +113,12 @@ class Classifier:
                 tmp = tmp * self.weightedprob(word,cat)
                # print tmp
         print("Prob of '%s' in %s : %s" %(item, cat, tmp * self.catprob(cat)))
-        return tmp * self.catprob(cat)
+
+        catprob = self.catprob(cat)
+
+        probresult = tmp * catprob
+
+        return probresult
 
 
 
@@ -114,11 +138,38 @@ doc1 = "This is a String with lower and UPPER case Letters, for testing our new 
 
 classifier = Classifier(getwords)
 
-corpus = [["nobody owns the water", "good"], ["the quick rabbit jumps fences", "good"], ["buy pharmaceuticals now","bad"], ["make quick money at the online casino","bad"], ["the quick brown fox jumps","good"] , ["next meeting is at night","good"], ["meeting with your superstar","good"] , ["money like water","bad"]]
+corpus = [["nobody owns the water", "good"],
+          ["the quick rabbit jumps fences", "good"],
+          ["buy pharmaceuticals now","bad"],
+          ["make quick money at the online casino","bad"],
+          ["the quick brown fox jumps","good"] ,
+          ["next meeting is at night","good"],
+          ["meeting with your superstar","good"] ,
+          ["money like water","bad"]]
 
 for sentence in corpus:
     classifier.train(sentence[0], sentence[1])
 
+
+print "________________________________________________________"
+
+print "Number of good documents: "
+print classifier.catcount("good")
+
+
+print "Number of bad documents: "
+print classifier.catcount("bad")
+
+
+print "Number of total documents: "
+print classifier.totalcount()
+
+
+print "________________________________________________________"
+
+
+
+# print(classifier.fprob("money", "bad"))
 
 #print classifier.fc
 #print classifier.cc
@@ -128,6 +179,9 @@ def showProb(string ):
     g = classifier.prob(string, "good")
     b = classifier.prob(string, "bad")
 
+    print g
+    print b
+
     probGood = g/(g+b)
     probBad = b/(g+b)
 
@@ -136,8 +190,8 @@ def showProb(string ):
     print("bad")
     print(probBad)
 
-print classifier.fc
-showProb(" money ")
+#print classifier.fc
+showProb("the money jumps")
 
 
 
