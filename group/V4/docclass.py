6778d6b2bece23d4c8a617798e9c5400d6724a1f
jackdecrack <jackdecrack@web.de>
2014-06-15 15:19:22 +0200

    aufgabe 2.4

--- group/V4/docclass.py
+++ group/V4/docclass.py
@@ -12,17 +12,11 @@ class Classifier:
 
     def incf(self,f,cat):
         #check f word is already in fc
-        if(self.fc.has_key(f)):
-            #check if word already has category cat
-            if(self.fc[f].has_key(cat)):
-                #increment counter
-                self.fc[f][cat] = self.fc[f][cat] + 1
-            else:
-                #category doesn't exist, add it with value 1
-                self.fc[f][cat] = 1
-        else:
-            #word and category don't exist, add both with value 1
-            self.fc[f][cat] = 1
+
+        if(self.fc.has_key(f)!=True):
+            self.fc[f] = {"good":0, "bad":0}
+
+        self.fc[f][cat] = self.fc[f][cat] + 1
 
     #increments category counter
     def incc(self,cat):
@@ -50,7 +44,7 @@ class Classifier:
     #train given sentence for given category
     def train(self,item,cat):
         #get words of sentence
-        words = self.getfeatures(item)
+        words = self.getfeatures(item,3,20)
         for word in words:
             #add each word to cf
             self.incf(word,cat)
@@ -60,25 +54,47 @@ class Classifier:
     #return probability of given word in given category
     def fprob(self,f,cat):
         if(self.catcount(cat) != 0):
-            return self.fcount(f,cat) / self.catcount(cat)
+            return float(self.fcount(f,cat)) / float(self.catcount(cat))
         else:
             return 0
 
     #add smoothing to avoid extreme probabilities
     def weightedprob(self,f,cat):
-        initprob = 0.5
-        return (initprob + self.fcount(f,cat) * self.fprob(f,cat))/1 + self.fcount(f,cat)
+
+        initprob = float(0.5)
+        fcount = float(self.fcount(f,cat))
+        fprob = float(self.fprob(f,cat))
+        print(fcount)
+        print(fprob)
+        result = float(initprob + fcount * fprob/(1 + fcount))
+
+        print("Prob of Word '%s' in %s : %s" %(f, cat, result))
+
+        return result
+
+    #return probability of given category
+    def catprob(self,cat):
+        amountCat = float(self.catcount(cat))
+        totalCount = float(self.totalcount())
+
+        return (amountCat / totalCount)
+
 
     #returns probability if given document is in given category
     def prob(self,item,cat):
-        words = self.getfeatures(item)
+        words = self.getfeatures(item, 3, 10)
         tmp = 0
         for word in words:
             if(tmp == 0):
+               # print "temp init"
                 tmp = self.weightedprob(word,cat)
+               # print tmp
+
             else:
                 tmp = tmp * self.weightedprob(word,cat)
-        return 0
+               # print tmp
+        print("Prob of '%s' in %s : %s" %(item, cat, tmp * self.catprob(cat)))
+        return tmp * self.catprob(cat)
 
 
 
@@ -93,7 +109,36 @@ def getwords(doc, minLength, maxLength):
 
 doc1 = "This is a String with lower and UPPER case Letters, for testing our new Algorithm. Maybe english is better than german, who knows. thatsaprettylongwordtotestifitgetsremovedinmyfuntion"
 
-res = getwords(doc1,3,20)
-print res
+#res = getwords(doc1,3,20)
+#print res
 
 classifier = Classifier(getwords)
+
+corpus = [["nobody owns the water", "good"], ["the quick rabbit jumps fences", "good"], ["buy pharmaceuticals now","bad"], ["make quick money at the online casino","bad"], ["the quick brown fox jumps","good"] , ["next meeting is at night","good"], ["meeting with your superstar","good"] , ["money like water","bad"]]
+
+for sentence in corpus:
+    classifier.train(sentence[0], sentence[1])
+
+
+#print classifier.fc
+#print classifier.cc
+
+
+def showProb(string ):
+    g = classifier.prob(string, "good")
+    b = classifier.prob(string, "bad")
+
+    probGood = g/(g+b)
+    probBad = b/(g+b)
+
+    print("good")
+    print(probGood)
+    print("bad")
+    print(probBad)
+
+print classifier.fc
+showProb(" money ")
+
+
+
+
